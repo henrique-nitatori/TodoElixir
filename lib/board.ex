ad25cc931ccaba6d3f4f9bad3e68ab56e501bda2
Henrique nitatori <henrique_nitatori@hotmail.com>
2020-12-02 12:55:04 -0300

    First commit

---
+++ lib/board.ex
@@ -0,0 +1,29 @@
+defmodule Board do
+  defstruct name: nil, items: []
+
+  def new(name, item \\ []) do
+    %Board{name: name, items: item}
+  end
+
+  def add_item_to_board(%{name: name, items: items}, new_item) do
+    new(name, [ new_item | items ])
+  end
+
+  def remove_item_from_board(%{name: name, items: items}, item_to_remove) do
+    new(name, Enum.reject(items, fn item -> item.description === item_to_remove.description end))
+  end
+
+  def mark_item_as_done(board, done_item) do
+    board
+      |> remove_item_from_board(done_item)
+      |> add_item_to_board(Item.done(done_item))
+
+  end
+
+  def mark_item_as_undone(board, undone_item) do
+    board
+      |> remove_item_from_board(undone_item)
+      |> add_item_to_board(Item.undone(undone_item))
+
+  end
+end
